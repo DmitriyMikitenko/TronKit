85217c73973f42e496f91263c2988a7dadf6e876
Dmitriy Mikitenko <>
2024-12-20 12:52:40 +0200

    fix commit

--- Sources/TronKit/Core/Kit.swift
+++ Sources/TronKit/Core/Kit.swift
@@ -124,7 +124,7 @@ public extension Kit {
         return newTransaction.txID
     }
     
-    func rawTransactionData(contract: Contract, signer: Signer, feeLimit: Int? = 0) async throws -> (CreatedTransactionResponse, Data) {
+    func rawTransactionData(contract: Contract, signer: Signer, feeLimit: Int? = 0) async throws -> String {//(CreatedTransactionResponse, Data) {
         try await transactionSender.rawTransaction(contract: contract, signer: signer, feeLimit: feeLimit)
     }
 
